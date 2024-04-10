5d6dc07d0d6bd93498579eac65aed3fc848772c0
Elian <eliandeniz27@gmail.com>
2024-04-10 10:40:44 +0200

    person file

---
+++ demos/src/05-setup.test.js
@@ -0,0 +1,21 @@
+describe('Set', () => {
+  test('case 1', () => {
+    console.log('case 1');
+    expect(1 + 1).toBe(2);
+  });
+  test('case 2', () => {
+    console.log('case 2');
+    expect(1 + 3).toBe(4);
+  });
+
+  describe('other group', () => {
+  test('case 3', () => {
+    console.log('case 3');
+    expect(1 + 4).toBe(5);
+  });
+  test('case 4', () => {
+    console.log('case 4');
+    expect(1+ 5).toBe(6);
+    });
+  });
+});
---
+++ demos/src/06-person.test.js
@@ -0,0 +1,23 @@
+const Person = require('./06-person');
+
+describe('Test for Person', () => {
+  let person;
+  beforeEach(() => {
+    person = new Person('Nicolas', 45, 1.7);
+  });
+
+  test('should return down', () => {
+    person.weight = 45;
+    const imc = person.calcIMCs();
+    expect(imc).toBe('down')
+  });
+
+  test('should return normal', () => {
+    person.weight = 59;
+    const person = new Person('Nicolas', 59, 1.7)
+    const imc = person.calcIMCs();
+    expect(imc).toBe('normal')
+  });
+})
+
+
